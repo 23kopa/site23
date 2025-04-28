6b2932bbcf93437b1fdbe2e5d341b2217a49d1b4
Ivan <134298973+23kopa@users.noreply.github.com>
2025-04-28 17:41:56 +0300

    style(ui): update application appearance and base styles

--- app.py
+++ app.py
@@ -1,59 +1,196 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request, session, jsonify
 import paramiko
 from aiogram import Bot, Dispatcher
+import os
+import psutil
+import socket
 
 app = Flask(__name__)
+app.secret_key = 'kopauser'
 
-# Настройки для бота
+# ! Настройки для бота
 API_TOKEN = '7505610899:AAF2RxxuifLLUhqprRvDDrOBs_-1TrdPADU'
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
-# Настройки для VPS (SSH)
+# ! Настройки для VPS (SSH)
 VPS_IP = '185.22.172.242'
 VPS_USER = 'root'
 VPS_PASSWORD = 'cN5tUXWYfj395b61Br'
 
 
-# Статистика бота
-@app.route('/')
-def home():
-    # Здесь можно выводить статистику, например, активных пользователей
-    return render_template('index.html')
+# ! SSH Connect to VPS
+def ssh_execute(command):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        ssh.connect(VPS_IP, username=VPS_USER, password=VPS_PASSWORD)
+        stdin, stdout, stderr = ssh.exec_command(command)
+        return stdout.read().decode(), stderr.read().decode()
+    except Exception as e:
+        return "", f"SSH error: {e}"
+    finally:
+        ssh.close()
+
+
+# ! Функции
+
+# Функция для получения использования CPU
+def get_cpu_usage():
+    # Используем psutil для получения процента использования CPU
+    cpu_usage = psutil.cpu_percent(interval=1)
+    return cpu_usage
+
+# Функция для получения количества ядер процессора
+def get_cpu_cores():
+    physical_cores = psutil.cpu_count(logical=False)  # физические ядра
+    logical_cores = psutil.cpu_count(logical=True)    # логические ядра
+    return physical_cores, logical_cores
+
+# Функция для получения частоты процессора
+def get_cpu_frequency():
+    freq = psutil.cpu_freq()
+    return freq.current, freq.min, freq.max  # Текущая, минимальная и максимальная частота процессора
+
+# Маршрут для получения данных о CPU
+@app.route('/cpu_usage')
+def cpu_usage():
+    cpu_usage = get_cpu_usage()
+    physical_cores, logical_cores = get_cpu_cores()
+    current_freq, min_freq, max_freq = get_cpu_frequency()
+
+    return jsonify({
+        'cpu_usage': cpu_usage,
+        'physical_cores': physical_cores,
+        'logical_cores': logical_cores,
+        'current_freq': current_freq,
+        'min_freq': min_freq,
+        'max_freq': max_freq
+    })
+
+# Функция для получения информации о сети
+def get_network_info():
+    net_io = psutil.net_io_counters()  # Получаем статистику по трафику
+    net_if_addrs = psutil.net_if_addrs()  # Получаем информацию об интерфейсах
+
+    network_info = {
+        'bytes': {
+            'sent': net_io.bytes_sent,  # Отправлено байтов
+            'recv': net_io.bytes_recv,  # Получено байтов
+        },
+        'packets': {
+            'sent': net_io.packets_sent,  # Отправлено пакетов
+            'recv': net_io.packets_recv,  # Получено пакетов
+        },
+    }
+    return network_info
+
+# Маршрут для получения информации о сети
+@app.route('/network_info')
+def network_info():
+    info = get_network_info()
+    return jsonify(info)
+
+# Disk Memory
+def get_disk_usage():
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        ssh.connect(VPS_IP, username=VPS_USER, password=VPS_PASSWORD)
+        # Используем команду du для получения размера директорий с исключением /proc
+        stdin, stdout, stderr = ssh.exec_command('du -h --exclude=/proc --max-depth=1 /')
+        disk_usage = stdout.read().decode()  # Чтение и декодирование вывода
+
+        # Преобразуем вывод в словарь для использования в шаблоне
+        usage_lines = disk_usage.splitlines()
+        usage_data = []
+        for line in usage_lines:
+            parts = line.split('\t')
+            if len(parts) == 2:
+                size, directory = parts
+                usage_data.append({'size': size, 'directory': directory})
+
+        return usage_data
+    except Exception as e:
+        return f"Ошибка при получении состояния диска: {e}"
+    finally:
+        ssh.close()
+
+
+# Статус Nginx
+@app.route('/nginx_status')
+def nginx_status():
+    command = 'systemctl is-active nginx'
+    stdout, stderr = ssh_execute(command)
+    if stderr:
+        return jsonify({'status': 'Ошибка при получении статуса Nginx'})
+    return jsonify({'status': stdout.strip()})
+
+
+# Статус SSL-сертификатов
+@app.route('/ssl_status')
+def ssl_status():
+    command = 'openssl x509 -in /etc/ssl/certs/botmanager.crt -noout -dates'  # Путь к вашему сертификату
+    stdout, stderr = ssh_execute(command)
+    if stderr:
+        return jsonify({'status': 'Ошибка при получении статуса SSL-сертификатов'})
+    return jsonify({'status': stdout.strip()})
 
 
 # Запуск бота на сервере
 @app.route('/start_bot')
 def start_bot():
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(VPS_IP, username=VPS_USER, password=VPS_PASSWORD)
-    ssh.exec_command('sudo systemctl start mybot')
-    ssh.close()
+    command = 'sudo systemctl start mybot'
+    stdout, stderr = ssh_execute(command)
+    if stderr:
+        # Обработка ошибки, если она есть
+        return f"Ошибка при запуске бота: {stderr}"
     return redirect(url_for('home'))
 
 
 # Остановка бота на сервере
 @app.route('/stop_bot')
 def stop_bot():
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(VPS_IP, username=VPS_USER, password=VPS_PASSWORD)
-    ssh.exec_command('sudo systemctl stop mybot')
-    ssh.close()
+    command = 'sudo systemctl stop mybot'
+    stdout, stderr = ssh_execute(command)
+    if stderr:
+        # Обработка ошибки, если она есть
+        return f"Ошибка при остановке бота: {stderr}"
     return redirect(url_for('home'))
 
 
 # Перезагрузка бота на сервере
 @app.route('/restart_bot')
 def restart_bot():
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(VPS_IP, username=VPS_USER, password=VPS_PASSWORD)
-    ssh.exec_command('sudo systemctl restart mybot')
-    ssh.close()
+    command = 'sudo systemctl restart mybot'
+    stdout, stderr = ssh_execute(command)
+    if stderr:
+        # Обработка ошибки, если она есть
+        return f"Ошибка при перезагрузке бота: {stderr}"
     return redirect(url_for('home'))
 
 
+@app.route('/logs')
+def logs():
+    stdout, stderr = ssh_execute('cat /var/log/syslog | tail -n 50')  # пример команды
+    logs_output = stdout if stdout else stderr
+    return render_template('test.html', logs=logs_output)
+
+@app.route('/bot')
+def botmanager():
+    return render_template('bot.html')
+
+
+# ! Запуск веб-приложения
+@app.route('/')
+def home():
+    # Здесь можно выводить статистику, например, активных пользователей
+    # active_processes = get_active_processes()
+    return render_template('index.html')
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(
+        host='0.0.0.0',
+        port=5000,
+        ssl_context=('/etc/letsencrypt/live/botmanager.site/fullchain.pem', '/etc/letsencrypt/live/botmanager.site/privkey.pem')
+    )
--- static/js/script.js
+++ static/js/script.js
@@ -1,25 +1,168 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Пример уведомления при запуске бота
-    const startButton = document.querySelector('a[href="/start_bot"]');
-    if (startButton) {
-        startButton.addEventListener('click', function() {
-            toastr.success('Бот успешно запущен!', 'Успех');
+  // Вызов функции при загрузке страницы
+  window.onload = function() {
+    fetchCpuUsage(); // Запросим данные о CPU
+    fetchNginxStatus(); // Запросим данные о CPU
+    fetchNetworkInfo();
+  };
+
+// Функция для запроса данных о диске
+function fetchDiskUsage() {
+    fetch('/get_disk_usage')
+        .then(response => response.json())  // Получаем данные в формате JSON
+        .then(data => {
+            // Формируем HTML для отображения данных
+            let content = '<ul>';
+            data.forEach(item => {
+                content += `<li><strong>Директория:</strong> ${item.directory} - <strong>Размер:</strong> ${item.size}</li>`;
+            });
+            content += '</ul>';
+            
+            // Вставляем данные в тело модального окна
+            document.getElementById('diskUsageContent').innerHTML = content;
+        })
+        .catch(error => {
+            console.error('Ошибка при получении данных:', error);
+            document.getElementById('diskUsageContent').innerHTML = 'Произошла ошибка при получении данных.';
         });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const terminalOutput = document.getElementById('terminal-output');
+    const textContainer = document.querySelector('.text');
+    const cursor = document.querySelector('.cursor');
+
+    const text = ">>> Добро пожаловать, бро! Тут без багов, только фичи <3";
+
+    let i = 0;
+    const typingSpeed = 100; // скорость печати (мс)
+
+    // Функция для печати текста
+    function typeText() {
+        if (i < text.length) {
+            textContainer.textContent += text.charAt(i); // добавляем символ в текст
+            i++;
+            setTimeout(typeText, typingSpeed);
+        } else {
+            // Когда текст полностью выведен, курсор мигает
+            cursor.style.animation = 'blink 0.7s step-start infinite';
+        }
     }
 
-    // Пример уведомления при остановке бота
-    const stopButton = document.querySelector('a[href="/stop_bot"]');
-    if (stopButton) {
-        stopButton.addEventListener('click', function() {
-            toastr.error('Бот был остановлен.', 'Ошибка');
-        });
-    }
-
-    // Пример уведомления при перезапуске бота
-    const restartButton = document.querySelector('a[href="/restart_bot"]');
-    if (restartButton) {
-        restartButton.addEventListener('click', function() {
-            toastr.info('Перезапуск бота...', 'Информация');
-        });
-    }
+    // Запуск анимации текста
+    typeText();
 });
+
+
+document.querySelectorAll('.btn').forEach(btn => {
+    btn.addEventListener('click', function (e) {
+      const ripple = document.createElement('span');
+      ripple.style.position = 'absolute';
+      ripple.style.borderRadius = '50%';
+      ripple.style.background = '#00ff88';
+      ripple.style.opacity = '0.6';
+      ripple.style.pointerEvents = 'none';
+      ripple.style.width = ripple.style.height = '100px';
+      ripple.style.transform = 'scale(0)';
+      ripple.style.animation = 'ripple 0.6s linear';
+  
+      const rect = this.getBoundingClientRect();
+      ripple.style.left = `${e.clientX - rect.left - 50}px`;
+      ripple.style.top = `${e.clientY - rect.top - 50}px`;
+  
+      this.appendChild(ripple);
+  
+      ripple.addEventListener('animationend', () => {
+        ripple.remove();
+      });
+    });
+  });
+
+  function fetchNginxStatus() {
+    fetch('/nginx_status')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('nginx-status').textContent = data.status;
+        });
+        
+}
+
+function fetchSslStatus() {
+    fetch('/ssl_status')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('ssl-status').textContent = data.status;
+        });
+}
+
+  // Функция для запроса информации о CPU
+// Функция для запроса информации о CPU
+function fetchCpuUsage() {
+    fetch('/cpu_usage')
+      .then(response => response.json())
+      .then(data => {
+        // Обновить элементы на странице с информацией о CPU
+        const cpuUsage = `Использование: ${data.cpu_usage}%`;
+        const cpuFrequency = `Частота: ${data.current_freq} MHz`;
+        const physicalCores = `Физические ядра: ${data.physical_cores}`;
+        const logicalCores = `Логические ядра: ${data.logical_cores}`;
+        
+        // Обновить вывод на страницу с разрывами строк для каждого параметра
+        document.getElementById('cpu-usage').innerHTML = `
+          ${cpuUsage}<br>
+          <hr>
+          ${cpuFrequency}<br>
+          ${physicalCores}<br>
+          ${logicalCores}
+        `;
+      })
+      .catch(error => {
+        console.error('Ошибка при запросе информации о CPU:', error);
+      });
+}
+
+function fetchNetworkInfo() {
+    fetch('/network_info')
+        .then(response => response.json())
+        .then(data => {
+            // Отображение байтов
+            document.getElementById('sent-bytes').textContent = data.bytes.sent;
+            document.getElementById('received-bytes').textContent = data.bytes.recv;
+
+            // Отображение пакетов
+            document.getElementById('sent-packets').textContent = data.packets.sent;
+            document.getElementById('received-packets').textContent = data.packets.recv;
+        })
+        .catch(error => {
+            console.error('Ошибка при запросе информации о сети:', error);
+        });
+}
+
+// Добавляем обработчик событий на каждую строку таблицы
+document.querySelectorAll('.table tr').forEach(row => {
+  row.addEventListener('click', function() {
+      // Находим ячейку <td> во второй колонке (адрес)
+      const cell = this.cells[1];  // Индекс 1 - вторая ячейка (адрес)
+      
+      // Создаем временный элемент input для копирования текста
+      const tempInput = document.createElement('input');
+      tempInput.value = cell.textContent;  // Устанавливаем значение в input
+      document.body.appendChild(tempInput);
+      
+      // Выбираем и копируем текст в input
+      tempInput.select();
+      document.execCommand('copy');
+      
+      // Удаляем временный input
+      document.body.removeChild(tempInput);
+
+      // Показать кастомное уведомление
+      const notification = document.getElementById('copyNotification');
+      notification.style.display = 'block';
+
+      // Скрыть уведомление через 2 секунды
+      setTimeout(() => {
+          notification.style.display = 'none';
+      }, 2000);
+  });
+});
+
