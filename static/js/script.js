69b8d12765c563babac51f6e628f9d0c2b3c22e0
Ivan <134298973+23kopa@users.noreply.github.com>
2025-05-20 03:19:52 +0300

    feat(auth): add user login form

--- static/js/script.js
+++ static/js/script.js
@@ -28,10 +28,11 @@ function fetchDiskUsage() {
 
 document.addEventListener("DOMContentLoaded", function () {
     const terminalOutput = document.getElementById('terminal-output');
-    const textContainer = document.querySelector('.text');
+    const textContainer = document.querySelector('.terminal-text');
     const cursor = document.querySelector('.cursor');
 
-    const text = ">>> Добро пожаловать, бро! Тут без багов, только фичи <3";
+    /* const text = ">>> Добро пожаловать, бро! Тут без багов, только фичи <3"; */
+    const text = ">> Добро пожаловать!";
 
     let i = 0;
     const typingSpeed = 100; // скорость печати (мс)
@@ -164,5 +165,45 @@ document.querySelectorAll('.table tr').forEach(row => {
           notification.style.display = 'none';
       }, 2000);
   });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (document.body.classList.contains("auth-page")) {
+    const c = document.getElementById("matrix");
+    if (!c) return;
+
+    const ctx = c.getContext("2d");
+    c.height = window.innerHeight;
+    c.width = window.innerWidth;
+
+    const letters = "アァイィウエカキクケコサシスセソ0123456789";
+    const fontSize = 18;
+    const columns = c.width / fontSize;
+    const drops = Array(Math.floor(columns)).fill(1);
+
+    function draw() {
+      ctx.fillStyle = "rgba(0, 0, 0, 0.1)";
+      ctx.fillRect(0, 0, c.width, c.height);
+
+      ctx.fillStyle = "rgba(0, 255, 0, 0.1)";
+      ctx.font = `${fontSize}px monospace`;
+
+      for (let i = 0; i < drops.length; i++) {
+        const text = letters[Math.floor(Math.random() * letters.length)];
+        ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+        if (drops[i] * fontSize > c.height && Math.random() > 0.995) drops[i] = 0;
+        drops[i]++;
+      }
+      setTimeout(() => requestAnimationFrame(draw), 50); // 50мс между кадрами = 20 FPS
+    }
+
+    draw();
+
+    // адаптация под ресайз окна
+    window.addEventListener("resize", () => {
+      c.width = window.innerWidth;
+      c.height = window.innerHeight;
+    });
+  }
 });
 
